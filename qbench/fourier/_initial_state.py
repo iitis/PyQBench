414b1167432e0e55c45b9c735490078129466f00
plewandowska777 <52824097+plewandowska777@users.noreply.github.com>
2022-01-21 17:01:00 +0100

    Pl/initial state preparation (#2)
    * Add qbench.fourier package
    
    * Implement initial state preparation for parametrized Fourier family
    
    * Add numpy and braket to install requirements
    
    * Make state_preparation_circuit a package-level function
    
    * Add docstring for state_preparation_circuit

---
+++ qbench/fourier/_initial_state.py
@@ -0,0 +1,11 @@
+from braket import circuits
+
+
+def state_preparation_circuit(target: int = 0, ancilla: int = 1) -> circuits.Circuit:
+    """Create circuit initializing system into maximally entangled state.
+
+    :param target: Index of qubit on which von Neumann measurement will be performed.
+    :param ancilla: Index of qubit on which conditional measurement will be performed.
+    :return: A circuit mapping |00> to (|00> + |11>) / sqrt(2).
+    """
+    return circuits.Circuit().h(target).cnot(target, ancilla)
---
+++ tests/test_fourier.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from qbench.fourier import state_preparation_circuit
+
+
+def test_initial_state_prepared_from_ket_zeros_is_maximally_entangled():
+    ket0 = np.array([1, 0, 0, 0])
+    circuit = state_preparation_circuit(0, 1)
+
+    np.testing.assert_allclose(circuit.as_unitary() @ ket0, [1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)])
