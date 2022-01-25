e02cea8e4eec44c4706ce43cb447ca748e13b9fd
dexter2206 <dexter2206@gmail.com>
2022-01-25 13:10:30 +0100

    Fix style issues in test file

--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from scipy import linalg
 
-from qbench.fourier import state_preparation_circuit, measurement_circuit
+from qbench.fourier import measurement_circuit, state_preparation_circuit
 
 
 def test_initial_state_prepared_from_ket_zeros_is_maximally_entangled():
@@ -12,7 +12,7 @@ def test_initial_state_prepared_from_ket_zeros_is_maximally_entangled():
     np.testing.assert_allclose(circuit.as_unitary() @ ket0, [1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)])
 
 
-@pytest.mark.parametrize("phi", [np.pi, np.pi/ 4, np.pi / 5, np.sqrt(2), 0])
+@pytest.mark.parametrize("phi", [np.pi, np.pi / 4, np.pi / 5, np.sqrt(2), 0])
 def test_measurement_circuit_has_correct_unitary(phi):
     circuit = measurement_circuit(phi)
     expected_unitary = linalg.dft(2) @ np.diag([1, np.exp(1j * phi)]) @ linalg.dft(2) / 2
