b5f94668d6cc1d8d5ef96c26cb1b4763ad1a4f3c
dexter2206 <dexter2206@gmail.com>
2022-06-06 15:41:24 +0200

    Draft interface of experiments

---
+++ qbench/direct.py
@@ -0,0 +1,5 @@
+"""Module implementing direct experiment."""
+
+
+def benchmark_directly(device, target, ancilla, state_preparation, v0_oplus_v1) -> None:
+    pass
---
+++ qbench/postselection.py
@@ -0,0 +1,5 @@
+"""Module implementing postselection experiment."""
+
+
+def benchmark_using_postselection(device, target, ancilla, state_preparation, v0, v1) -> None:
+    pass
