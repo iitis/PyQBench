4fca7f43005055fbb127ad8078fc10781c9e6505
plewandowska <plewandowska@iitis.pl>
2022-06-07 15:53:46 +0200

    Add typing to signatures

--- qbench/postselection.py
+++ qbench/postselection.py
@@ -1,5 +1,16 @@
 """Module implementing postselection experiment."""
+from typing import Callable
+
+from braket import circuits, devices
 
 
-def benchmark_using_postselection(device, target, ancilla, state_preparation, v0, v1) -> None:
+def benchmark_using_postselection(
+    device: devices.Device,
+    target: int,
+    ancilla: int,
+    state_preparation: Callable[[int, int], circuits.Circuit],
+    v0: Callable[[int, int], circuits.Circuit],
+    v1: Callable[[int, int], circuits.Circuit],
+) -> float:
+
     pass
