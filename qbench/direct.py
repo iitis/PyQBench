6567b60ea51873f4858c8850d9e8dbe2f584e370
plewandowska <plewandowska@iitis.pl>
2022-06-07 15:49:20 +0200

    Typing of variables

--- qbench/direct.py
+++ qbench/direct.py
@@ -1,7 +1,14 @@
 """Module implementing direct experiment."""
+from typing import Callable
+
+from braket import circuits, devices
 
 
 def benchmark_using_controlled_unitary(
-    device, target, ancilla, state_preparation, controlled_unitary
-) -> None:
+    device: devices.Device,
+    target: int,
+    ancilla: int,
+    state_preparation: Callable[[int, int], circuits.Circuit],
+    controlled_unitary: Callable[[int, int], circuits.Circuit],
+) -> float:
     pass
