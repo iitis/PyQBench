333c8e5e5db64ea30ec7656dee45f0b2f6def5d7
dexter2206 <dexter2206@gmail.com>
2022-06-08 18:56:24 +0200

    Add unit test for benchmarking using controlled unitaries
    Co-authored-by: plewandowska777 <plewandowska@iitis.pl>

--- tests/test_direct.py
+++ tests/test_direct.py
@@ -1,8 +1,14 @@
 from collections import Counter
 
+import numpy as np
 import pytest
+from braket import devices
 
-from qbench.direct import count_specific_measurements
+from qbench import fourier
+from qbench.direct import (
+    benchmark_using_controlled_unitary,
+    count_specific_measurements,
+)
 
 
 @pytest.mark.parametrize(
@@ -19,3 +25,21 @@ def test_counting_specific_measurements(
     assert (
         count_specific_measurements(measurement_counts, qubit_index, qubit_value) == expected_count
     )
+
+
+@pytest.mark.parametrize("phi", np.linspace(0, 2 * np.pi, 100))
+@pytest.mark.parametrize("native_only", [False, True])
+def test_computed_discrimination_probability_is_feasible(phi: float, native_only):
+    device = devices.LocalSimulator()
+
+    probability = benchmark_using_controlled_unitary(
+        device=device,
+        target=0,
+        ancilla=1,
+        state_preparation=fourier.state_preparation,
+        basis_change=fourier.unitary_to_discriminate(phi),
+        controlled_unitary=fourier.controlled_v0_v1_dag(phi, native_only=native_only),
+        num_shots_per_measurement=20,
+    )
+
+    assert 0 <= probability <= 1
