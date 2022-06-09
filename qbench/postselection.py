d644c4a0c09b1266068169564d362b33fce1d2c2
plewandowska <plewandowska@iitis.pl>
2022-06-09 09:41:38 +0200

    Adding postselection (with shorter probability formula) with usage of it

--- qbench/postselection.py
+++ qbench/postselection.py
@@ -1,7 +1,15 @@
 """Module implementing postselection experiment."""
-from typing import Callable
+from typing import Callable, Counter
 
 from braket import circuits, devices
+
+
+def count_specific_measurements(measurement_counts: Counter, qubit_index: int, qubit_value: int):
+    return sum(
+        count
+        for bitstring, count in measurement_counts.items()
+        if int(bitstring[qubit_index]) == qubit_value
+    )
 
 
 def benchmark_using_postselection(
@@ -10,9 +18,26 @@ def benchmark_using_postselection(
     ancilla: int,
     state_preparation: Callable[[int, int], circuits.Circuit],
     basis_change: Callable[[int], circuits.Circuit],
-    v0: Callable[[int, int], circuits.Circuit],
-    v1: Callable[[int, int], circuits.Circuit],
+    v0: Callable[[int], circuits.Circuit],
+    v1: Callable[[int], circuits.Circuit],
     num_shots_per_measurement: int,
 ) -> float:
 
-    pass
+    identity_circuit = state_preparation(target, ancilla) + v1(ancilla)
+
+    u_circuit = state_preparation(target, ancilla) + basis_change(target) + v0(ancilla)
+
+    identity_results = device.run(identity_circuit, shots=num_shots_per_measurement).result()
+    u_results = device.run(u_circuit, shots=num_shots_per_measurement).result()
+
+    return (u_results.measurement_counts["00"]) / (
+        2
+        * count_specific_measurements(
+            u_results.measurement_counts, u_results.measured_qubits.index(ancilla), 0
+        )
+    ) + (identity_results.measurement_counts["11"]) / (
+        2
+        * count_specific_measurements(
+            identity_results.measurement_counts, identity_results.measured_qubits.index(ancilla), 1
+        )
+    )
