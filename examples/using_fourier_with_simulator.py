cddfbb756831b9695249d284c284dcc6acba45af
dexter2206 <dexter2206@gmail.com>
2022-06-13 15:04:33 +0200

    Tidy up examples
    Co-authored-by: plewandowska777 <plewandowska@iitis.pl>

--- examples/using_fourier_with_simulator.py
+++ examples/using_fourier_with_simulator.py
@@ -15,16 +15,6 @@ def main():
     device = devices.LocalSimulator(backend="braket_dm")
     phis = np.linspace(0, 2 * np.pi, 100)
 
-    p = benchmark_using_controlled_unitary(
-        device=device,
-        target=TARGET,
-        ancilla=ANCILLA,
-        state_preparation=fourier.state_preparation,
-        basis_change=fourier.unitary_to_discriminate(np.pi / 2),
-        controlled_unitary=fourier.controlled_v0_v1_dag(np.pi / 2, native_only=NATIVE_ONLY),
-        num_shots_per_measurement=NUM_SHOTS_PER_MEASUREMENT,
-    )
-
     theoretical_probs = fourier.discrimination_probability_upper_bound(phis)
 
     actual_probs = [
