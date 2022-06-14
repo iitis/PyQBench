06ac773163ddccda14c23ed093fcdd02d4a496d7
Konrad Jałowiecki <dexter2206@gmail.com>
2022-06-15 01:46:39 +0200

    Update the examples

--- examples/using_fourier_with_simulator.py
+++ examples/using_fourier_with_simulator.py
@@ -2,8 +2,8 @@ import numpy as np
 from braket import devices
 from matplotlib import pyplot as plt
 
-from qbench import fourier
 from qbench.direct import benchmark_using_controlled_unitary
+from qbench.fourier import FourierCircuits, discrimination_probability_upper_bound
 
 NUM_SHOTS_PER_MEASUREMENT = 1000
 TARGET = 0
@@ -15,19 +15,19 @@ def main():
     device = devices.LocalSimulator(backend="braket_dm")
     phis = np.linspace(0, 2 * np.pi, 100)
 
-    theoretical_probs = fourier.discrimination_probability_upper_bound(phis)
+    theoretical_probs = discrimination_probability_upper_bound(phis)
 
     actual_probs = [
         benchmark_using_controlled_unitary(
             device=device,
             target=TARGET,
             ancilla=ANCILLA,
-            state_preparation=fourier.state_preparation,
-            basis_change=fourier.unitary_to_discriminate(phi),
-            controlled_unitary=fourier.controlled_v0_v1_dag(phi, native_only=NATIVE_ONLY),
+            state_preparation=circuits.state_preparation,
+            basis_change=circuits.unitary_to_discriminate,
+            controlled_unitary=circuits.controlled_v0_v1_dag,
             num_shots_per_measurement=NUM_SHOTS_PER_MEASUREMENT,
         )
-        for phi in phis
+        for circuits in (FourierCircuits(phi, native_only=NATIVE_ONLY) for phi in phis)
     ]
 
     fig, ax = plt.subplots()
--- examples/using_fourier_with_simulator_postselection_all_cases.py
+++ examples/using_fourier_with_simulator_postselection_all_cases.py
@@ -2,7 +2,7 @@ import numpy as np
 from braket import devices
 from matplotlib import pyplot as plt
 
-from qbench import fourier
+from qbench.fourier import FourierCircuits, discrimination_probability_upper_bound
 from qbench.postselection_all_cases import benchmark_using_postselection_all_cases
 
 NUM_SHOTS_PER_MEASUREMENT = 10000
@@ -15,20 +15,20 @@ def main():
     device = devices.LocalSimulator(backend="braket_dm")
     phis = np.linspace(0, 2 * np.pi, 100)
 
-    theoretical_probs = fourier.discrimination_probability_upper_bound(phis)
+    theoretical_probs = discrimination_probability_upper_bound(phis)
 
     actual_probs = [
         benchmark_using_postselection_all_cases(
             device=device,
             target=TARGET,
             ancilla=ANCILLA,
-            state_preparation=fourier.state_preparation,
-            basis_change=fourier.unitary_to_discriminate(phi),
-            v0=fourier.v0_dag(phi, native_only=NATIVE_ONLY),
-            v1=fourier.v1_dag(phi, native_only=NATIVE_ONLY),
+            state_preparation=circuits.state_preparation,
+            basis_change=circuits.unitary_to_discriminate,
+            v0=circuits.v0_dag,
+            v1=circuits.v1_dag,
             num_shots_per_measurement=NUM_SHOTS_PER_MEASUREMENT,
         )
-        for phi in phis
+        for circuits in (FourierCircuits(phi, native_only=NATIVE_ONLY) for phi in phis)
     ]
 
     fig, ax = plt.subplots()
