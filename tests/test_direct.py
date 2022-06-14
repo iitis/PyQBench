fb92ee3cea49574c249617581e60227d94ef03b5
Konrad Jałowiecki <dexter2206@gmail.com>
2022-06-15 01:54:34 +0200

    Fix tests broken because of the API change

--- tests/test_direct.py
+++ tests/test_direct.py
@@ -2,22 +2,23 @@ import numpy as np
 import pytest
 from braket import devices
 
-from qbench import fourier
 from qbench.direct import benchmark_using_controlled_unitary
+from qbench.fourier import FourierCircuits
 
 
 @pytest.mark.parametrize("phi", np.linspace(0, 2 * np.pi, 100))
 @pytest.mark.parametrize("native_only", [False, True])
 def test_computed_discrimination_probability_is_feasible(phi: float, native_only):
     device = devices.LocalSimulator()
+    circuits = FourierCircuits(phi=phi, native_only=native_only)
 
     probability = benchmark_using_controlled_unitary(
         device=device,
         target=0,
         ancilla=1,
-        state_preparation=fourier.state_preparation,
-        basis_change=fourier.unitary_to_discriminate(phi),
-        controlled_unitary=fourier.controlled_v0_v1_dag(phi, native_only=native_only),
+        state_preparation=circuits.state_preparation,
+        basis_change=circuits.unitary_to_discriminate,
+        controlled_unitary=circuits.controlled_v0_v1_dag,
         num_shots_per_measurement=20,
     )
 
--- tests/test_postselection.py
+++ tests/test_postselection.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from braket import devices
 
-from qbench import fourier
+from qbench.fourier import FourierCircuits
 from qbench.postselection import benchmark_using_postselection
 
 
@@ -10,15 +10,16 @@ from qbench.postselection import benchmark_using_postselection
 @pytest.mark.parametrize("native_only", [False, True])
 def test_computed_discrimination_probability_is_feasible(phi: float, native_only):
     device = devices.LocalSimulator()
+    circuits = FourierCircuits(phi=phi, native_only=native_only)
 
     probability = benchmark_using_postselection(
         device=device,
         target=0,
         ancilla=1,
-        state_preparation=fourier.state_preparation,
-        basis_change=fourier.unitary_to_discriminate(phi),
-        v0=fourier.v0_dag(phi, native_only=native_only),
-        v1=fourier.v1_dag(phi, native_only=native_only),
+        state_preparation=circuits.state_preparation,
+        basis_change=circuits.unitary_to_discriminate,
+        v0=circuits.v0_dag,
+        v1=circuits.v1_dag,
         num_shots_per_measurement=20,
     )
 
