d0bf7f4fb179d5023393a4be696f52b335c79c4f
Konrad Jałowiecki <dexter2206@gmail.com>
2022-06-15 01:27:58 +0200

    Implement all fourier circuits in a single class

--- qbench/fourier.py
+++ qbench/fourier.py
@@ -1,126 +1,112 @@
-from typing import Callable, Union
+from typing import Union
 
 import numpy as np
 from braket import circuits
 
 
-def state_preparation(target: int = 0, ancilla: int = 1) -> circuits.Circuit:
-    """Create circuit initializing system into maximally entangled state.
+class FourierCircuits:
+    def __init__(self, phi: float, native_only: bool = False):
+        self.phi = phi
+        self.native_only = native_only
 
-    .. note::
-       The returned circuit is (assuming target=0, ancilla=1) of the form:
+    @staticmethod
+    def state_preparation(target: int, ancilla: int) -> circuits.Circuit:
+        """Create circuit initializing system into maximally entangled state.
 
-       0: ───H───@───
-                 │
-       1: ───────X───
+        .. note::
+           The returned circuit is (assuming target=0, ancilla=1) of the form:
 
-    :param target: Index of qubit on which von Neumann measurement will be performed.
-    :param ancilla: Index of qubit on which conditional measurement will be performed.
-    :return: A circuit mapping |00> to (|00> + |11>) / sqrt(2).
-    """
-    return circuits.Circuit().h(target).cnot(target, ancilla)
+           0: ───H───@───
+                     │
+           1: ───────X───
 
+        :return: A circuit mapping |00> to (|00> + |11>) / sqrt(2).
+        """
+        return circuits.Circuit().h(target).cnot(target, ancilla)
 
-def unitary_to_discriminate(phi: float) -> Callable[[int], circuits.Circuit]:
-    """Create function that produces a unitary channel parametrized by angle phi.
+    def unitary_to_discriminate(self, qubit: int) -> circuits.Circuit:
+        """Create function that produces a unitary channel parametrized by angle phi.
 
-    .. note::
-       The returned circuits can be viewed as a change of basis in which von Neumann
-       measurement is to be performed and it looks as follows (assuming target=0).
+        .. note::
+           The returned circuits can be viewed as a change of basis in which von Neumann
+           measurement is to be performed and it looks as follows (assuming target=0).
 
-       0: ───H───Phase(-ϕ)───H───
+           0: ───H───Phase(-ϕ)───H───
 
-    :param phi: Rotation angle used in PHASE gate.
-    :return: A function mapping qubit index (target) to circuit implementing
-     appropriate unitary channel.
-    """
+        :param phi: Rotation angle used in PHASE gate.
+        :return: A function mapping qubit index (target) to circuit implementing
+         appropriate unitary channel.
+        """
 
-    def _circuit_factory(target: int) -> circuits.Circuit:
-        return circuits.Circuit().h(target).phaseshift(target, -phi).h(target)
+        return circuits.Circuit().h(qubit).phaseshift(qubit, -self.phi).h(qubit)
 
-    return _circuit_factory
+    def v0_dag(self, qubit: int) -> circuits.Circuit:
+        """Return function producing positive part of Holevo-Helstrom measurement.
 
-
-def v0_dag(phi: float, native_only: bool = False) -> Callable[[int], circuits.Circuit]:
-    """Return function producing positive part of Holevo-Helstrom measurement.
-
-    :param phi: rotation angle.
-    :param native_only: use only gates native to current Rigetti QPUs. Use this if you
-     don't trust the compiler.
-    :return: Function mapping qubit to a circuit implementing positive part of
-     Holevo-Helstrom measurement.
-    """
-
-    def _circuit_factory(target: int) -> circuits.Circuit:
+        :param phi: rotation angle.
+        :param native_only: use only gates native to current Rigetti QPUs. Use this if you
+         don't trust the compiler.
+        :return: Function mapping qubit to a circuit implementing positive part of
+         Holevo-Helstrom measurement.
+        """
         return (
             circuits.Circuit()
-            .rz(target, -np.pi / 2)
-            .rx(target, np.pi / 2)
-            .rz(target, -(phi + np.pi) / 2)
-            .rx(target, -np.pi / 2)
-            if native_only
-            else (circuits.Circuit().rz(target, -np.pi / 2).ry(target, -(phi + np.pi) / 2))
+            .rz(qubit, -np.pi / 2)
+            .rx(qubit, np.pi / 2)
+            .rz(qubit, -(self.phi + np.pi) / 2)
+            .rx(qubit, -np.pi / 2)
+            if self.native_only
+            else (circuits.Circuit().rz(qubit, -np.pi / 2).ry(qubit, -(self.phi + np.pi) / 2))
         )
 
-    return _circuit_factory
+    def v1_dag(self, qubit) -> circuits.Circuit:
+        """Return function producing positive part of Holevo-Helstrom measurement.
 
+        :param phi: rotation angle.
+        :param native_only: use only gates native to current Rigetti QPUs. Use this if you
+         don't trust the compiler.
+        :return: Function mapping qubit to a circuit implementing positive part of
+         Holevo-Helstrom measurement.
+        """
 
-def v1_dag(phi: float, native_only: bool = False) -> Callable[[int], circuits.Circuit]:
-    """Return function producing positive part of Holevo-Helstrom measurement.
-
-    :param phi: rotation angle.
-    :param native_only: use only gates native to current Rigetti QPUs. Use this if you
-     don't trust the compiler.
-    :return: Function mapping qubit to a circuit implementing positive part of
-     Holevo-Helstrom measurement.
-    """
-
-    def _circuit_factory(target: int) -> circuits.Circuit:
         return (
             circuits.Circuit()
-            .rz(target, np.pi / 2)
-            .rx(target, np.pi / 2)
-            .rz(target, -(np.pi - phi) / 2)
-            .rx(target, -np.pi / 2)
-            if native_only
+            .rz(qubit, np.pi / 2)
+            .rx(qubit, np.pi / 2)
+            .rz(qubit, -(np.pi - self.phi) / 2)
+            .rx(qubit, -np.pi / 2)
+            if self.native_only
             else circuits.Circuit()
-            .rz(target, -np.pi / 2)
-            .ry(target, -(phi + np.pi) / 2)
-            .rx(target, -np.pi)
+            .rz(qubit, -np.pi / 2)
+            .ry(qubit, -(self.phi + np.pi) / 2)
+            .rx(qubit, -np.pi)
         )
 
-    return _circuit_factory
+    def controlled_v0_v1_dag(self, target: int, ancilla: int) -> circuits.Circuit:
+        """Create a function producing controlled Holevo-Helstrom measurement.
 
+        .. note::
+           In usual basis ordering, the unitaries produced by this function would be
+           block-diagonal, with blocks corresponding to positive and negative parts
+           of Holevo-Helstrom measurement.
 
-def controlled_v0_v1_dag(phi, native_only: bool = True):
-    """Create a function producing controlled Holevo-Helstrom measurement.
+           However, Braket enumerates basis vectors in reverse, so the produced unitaries
+           are not block-diagonal, unless the qubits are swapped.
+           See accompanying tests to see how it's done.
 
-    .. note::
-       In usual basis ordering, the unitaries produced by this function would be
-       block-diagonal, with blocks corresponding to positive and negative parts
-       of Holevo-Helstrom measurement.
+           The following article contains more details on basis vectors ordering used
+           (among others) by Braket:
+           https://arxiv.org/abs/1711.02086
 
-       However, Braket enumerates basis vectors in reverse, so the produced unitaries
-       are not block-diagonal, unless the qubits are swapped.
-       See accompanying tests to see how it's done.
-
-       The following article contains more details on basis vectors ordering used
-       (among others) by Braket:
-       https://arxiv.org/abs/1711.02086
-
-    :param phi: rotation angle for positive and negative part.
-    :param native_only: use only gates native to Rigetti architecture.
-    :return: Circuit implementing V0 \\oplus V1.
-    """
-
-    def _circuit_factory(target, ancilla) -> circuits.Circuit:
+        :param phi: rotation angle for positive and negative part.
+        :param native_only: use only gates native to Rigetti architecture.
+        :return: Circuit implementing V0 \\oplus V1.
+        """
         return (
             circuits.Circuit().phaseshift(target, np.pi)
-            + v0_dag(phi, native_only=native_only)(ancilla)
+            + self.v0_dag(ancilla)
             + circuits.Circuit().cnot(target, ancilla)
         )
-
-    return _circuit_factory
 
 
 def discrimination_probability_upper_bound(
--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -2,14 +2,7 @@ import numpy as np
 import pytest
 from scipy import linalg
 
-from qbench.fourier import (
-    controlled_v0_v1_dag,
-    discrimination_probability_upper_bound,
-    state_preparation,
-    unitary_to_discriminate,
-    v0_dag,
-    v1_dag,
-)
+from qbench.fourier import FourierCircuits, discrimination_probability_upper_bound
 
 
 def _assert_unitaries_equal_up_to_phase(actual, expected):
@@ -58,14 +51,14 @@ def _v0_v1_block_diag_ref(phi):
 
 def test_initial_state_prepared_from_ket_zeros_is_maximally_entangled():
     ket0 = np.array([1, 0, 0, 0])
-    circuit = state_preparation(0, 1)
+    circuit = FourierCircuits(phi=0.1).state_preparation(0, 1)
 
     np.testing.assert_allclose(circuit.as_unitary() @ ket0, [1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)])
 
 
 @pytest.mark.parametrize("phi", [np.pi, np.pi / 4, np.pi / 5, np.sqrt(2), 0])
 def test_measurement_circuit_has_correct_unitary(phi):
-    circuit = unitary_to_discriminate(phi)(0)
+    circuit = FourierCircuits(phi=phi).unitary_to_discriminate(0)
     expected_unitary = linalg.dft(2) @ np.diag([1, np.exp(-1j * phi)]) @ linalg.dft(2) / 2
 
     np.testing.assert_allclose(circuit.as_unitary(), expected_unitary, atol=1e-6)
@@ -74,7 +67,7 @@ def test_measurement_circuit_has_correct_unitary(phi):
 @pytest.mark.parametrize("native_only", [True, False])
 @pytest.mark.parametrize("phi", np.linspace(0, 2 * np.pi, 100))
 def test_decomposed_v0_dagger_is_equal_to_the_original_one(phi: float, native_only):
-    actual = v0_dag(phi, native_only=native_only)(0).as_unitary()
+    actual = FourierCircuits(phi=phi, native_only=native_only).v0_dag(0).as_unitary()
     expected = _v0_ref(phi).conj().T
 
     _assert_unitaries_equal_up_to_phase(actual, expected)
@@ -83,7 +76,7 @@ def test_decomposed_v0_dagger_is_equal_to_the_original_one(phi: float, native_on
 @pytest.mark.parametrize("native_only", [True, False])
 @pytest.mark.parametrize("phi", np.linspace(0, 2 * np.pi, 100))
 def test_decomposed_v1_is_equal_to_the_original_one(phi: float, native_only):
-    actual = v1_dag(phi, native_only=native_only)(0).as_unitary()
+    actual = FourierCircuits(phi=phi, native_only=native_only).v1_dag(0).as_unitary()
     expected = _v1_ref(phi).conj().T
 
     _assert_unitaries_equal_up_to_phase(actual, expected)
@@ -92,7 +85,9 @@ def test_decomposed_v1_is_equal_to_the_original_one(phi: float, native_only):
 @pytest.mark.parametrize("native_only", [True, False])
 @pytest.mark.parametrize("phi", np.linspace(0, 2 * np.pi, 100))
 def test_decomposed_v0_v1_circuit_is_equal_to_the_original_one_up_to_phase(phi, native_only):
-    actual = controlled_v0_v1_dag(phi, native_only=native_only)(0, 1).as_unitary()
+    actual = (
+        FourierCircuits(phi=phi, native_only=native_only).controlled_v0_v1_dag(0, 1).as_unitary()
+    )
     expected = _v0_v1_block_diag_ref(phi).conj().T
 
     _assert_unitaries_equal_up_to_phase(actual, expected)
