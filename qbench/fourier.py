47d7b1ab67fe11bea188f557cd9cf18d53166ba8
Dexter <dexter2206@gmail.com>
2022-06-15 13:00:30 +0200

    Fourier circuits as class (#19)
    * Implement all fourier circuits in a single class
    
    * Update docstrings
    
    * Update the examples
    
    * Fix tests broken because of the API change

--- qbench/fourier.py
+++ qbench/fourier.py
@@ -1,126 +1,108 @@
-from typing import Callable, Union
+from typing import Union
 
 import numpy as np
 from braket import circuits
 
 
-def state_preparation(target: int = 0, ancilla: int = 1) -> circuits.Circuit:
-    """Create circuit initializing system into maximally entangled state.
+class FourierCircuits:
+    """Class creating circuits for Fourier-measurement experiment.
 
-    .. note::
-       The returned circuit is (assuming target=0, ancilla=1) of the form:
-
-       0: ───H───@───
-                 │
-       1: ───────X───
-
-    :param target: Index of qubit on which von Neumann measurement will be performed.
-    :param ancilla: Index of qubit on which conditional measurement will be performed.
-    :return: A circuit mapping |00> to (|00> + |11>) / sqrt(2).
-    """
-    return circuits.Circuit().h(target).cnot(target, ancilla)
-
-
-def unitary_to_discriminate(phi: float) -> Callable[[int], circuits.Circuit]:
-    """Create function that produces a unitary channel parametrized by angle phi.
-
-    .. note::
-       The returned circuits can be viewed as a change of basis in which von Neumann
-       measurement is to be performed and it looks as follows (assuming target=0).
-
-       0: ───H───Phase(-ϕ)───H───
-
-    :param phi: Rotation angle used in PHASE gate.
-    :return: A function mapping qubit index (target) to circuit implementing
-     appropriate unitary channel.
+    :param phi: Fourier angle of measurement to discriminate.
+    :param native_only: whether to only use gates native to Rigetti architecture.
+     Defaults to faults, in which case gates in the circuits will be compiled by
+     Braket.
     """
 
-    def _circuit_factory(target: int) -> circuits.Circuit:
-        return circuits.Circuit().h(target).phaseshift(target, -phi).h(target)
+    def __init__(self, phi: float, native_only: bool = False):
+        self.phi = phi
+        self.native_only = native_only
 
-    return _circuit_factory
+    @staticmethod
+    def state_preparation(target: int, ancilla: int) -> circuits.Circuit:
+        """Create circuit initializing system into maximally entangled state.
 
+        .. note::
+           The returned circuit is (assuming target=0, ancilla=1) of the form:
 
-def v0_dag(phi: float, native_only: bool = False) -> Callable[[int], circuits.Circuit]:
-    """Return function producing positive part of Holevo-Helstrom measurement.
+           0: ───H───@───
+                     │
+           1: ───────X───
 
-    :param phi: rotation angle.
-    :param native_only: use only gates native to current Rigetti QPUs. Use this if you
-     don't trust the compiler.
-    :return: Function mapping qubit to a circuit implementing positive part of
-     Holevo-Helstrom measurement.
-    """
+        :return: A circuit mapping |00> to (|00> + |11>) / sqrt(2).
+        """
+        return circuits.Circuit().h(target).cnot(target, ancilla)
 
-    def _circuit_factory(target: int) -> circuits.Circuit:
+    def unitary_to_discriminate(self, qubit: int) -> circuits.Circuit:
+        """Create a unitary channel corresponding to the measurement to discriminate.
+
+        .. note::
+           The returned circuits can be viewed as a change of basis in which von Neumann
+           measurement is to be performed, and it looks as follows (assuming qubit=0).
+
+           0: ───H───Phase(-ϕ)───H───
+
+        :return: A circuit implementing appropriate unitary channel.
+        """
+
+        return circuits.Circuit().h(qubit).phaseshift(qubit, -self.phi).h(qubit)
+
+    def v0_dag(self, qubit: int) -> circuits.Circuit:
+        """Create circuit corresponding to the positive part of Holevo-Helstrom measurement.
+
+        :return: A circuit implementing positive part of Holevo-Helstrom measurement.
+        """
         return (
             circuits.Circuit()
-            .rz(target, -np.pi / 2)
-            .rx(target, np.pi / 2)
-            .rz(target, -(phi + np.pi) / 2)
-            .rx(target, -np.pi / 2)
-            if native_only
-            else (circuits.Circuit().rz(target, -np.pi / 2).ry(target, -(phi + np.pi) / 2))
+            .rz(qubit, -np.pi / 2)
+            .rx(qubit, np.pi / 2)
+            .rz(qubit, -(self.phi + np.pi) / 2)
+            .rx(qubit, -np.pi / 2)
+            if self.native_only
+            else (circuits.Circuit().rz(qubit, -np.pi / 2).ry(qubit, -(self.phi + np.pi) / 2))
         )
 
-    return _circuit_factory
+    def v1_dag(self, qubit) -> circuits.Circuit:
+        """Create circuit corresponding to the negative part of Holevo-Helstrom measurement.
 
+        :return: A circuit implementing positive part of Holevo-Helstrom measurement.
+        """
 
-def v1_dag(phi: float, native_only: bool = False) -> Callable[[int], circuits.Circuit]:
-    """Return function producing positive part of Holevo-Helstrom measurement.
-
-    :param phi: rotation angle.
-    :param native_only: use only gates native to current Rigetti QPUs. Use this if you
-     don't trust the compiler.
-    :return: Function mapping qubit to a circuit implementing positive part of
-     Holevo-Helstrom measurement.
-    """
-
-    def _circuit_factory(target: int) -> circuits.Circuit:
         return (
             circuits.Circuit()
-            .rz(target, np.pi / 2)
-            .rx(target, np.pi / 2)
-            .rz(target, -(np.pi - phi) / 2)
-            .rx(target, -np.pi / 2)
-            if native_only
+            .rz(qubit, np.pi / 2)
+            .rx(qubit, np.pi / 2)
+            .rz(qubit, -(np.pi - self.phi) / 2)
+            .rx(qubit, -np.pi / 2)
+            if self.native_only
             else circuits.Circuit()
-            .rz(target, -np.pi / 2)
-            .ry(target, -(phi + np.pi) / 2)
-            .rx(target, -np.pi)
+            .rz(qubit, -np.pi / 2)
+            .ry(qubit, -(self.phi + np.pi) / 2)
+            .rx(qubit, -np.pi)
         )
 
-    return _circuit_factory
+    def controlled_v0_v1_dag(self, target: int, ancilla: int) -> circuits.Circuit:
+        """Create circuit implementing controlled Holevo-Helstrom measurement.
 
+        .. note::
+           In usual basis ordering, the unitaries produced by this function would be
+           block-diagonal, with blocks corresponding to positive and negative parts
+           of Holevo-Helstrom measurement.
 
-def controlled_v0_v1_dag(phi, native_only: bool = True):
-    """Create a function producing controlled Holevo-Helstrom measurement.
+           However, Braket enumerates basis vectors in reverse, so the produced unitaries
+           are not block-diagonal, unless the qubits are swapped.
+           See accompanying tests to see how it's done.
 
-    .. note::
-       In usual basis ordering, the unitaries produced by this function would be
-       block-diagonal, with blocks corresponding to positive and negative parts
-       of Holevo-Helstrom measurement.
+           The following article contains more details on basis vectors ordering used
+           (among others) by Braket:
+           https://arxiv.org/abs/1711.02086
 
-       However, Braket enumerates basis vectors in reverse, so the produced unitaries
-       are not block-diagonal, unless the qubits are swapped.
-       See accompanying tests to see how it's done.
-
-       The following article contains more details on basis vectors ordering used
-       (among others) by Braket:
-       https://arxiv.org/abs/1711.02086
-
-    :param phi: rotation angle for positive and negative part.
-    :param native_only: use only gates native to Rigetti architecture.
-    :return: Circuit implementing V0 \\oplus V1.
-    """
-
-    def _circuit_factory(target, ancilla) -> circuits.Circuit:
+        :return: Circuit implementing V0 \\oplus V1.
+        """
         return (
             circuits.Circuit().phaseshift(target, np.pi)
-            + v0_dag(phi, native_only=native_only)(ancilla)
+            + self.v0_dag(ancilla)
             + circuits.Circuit().cnot(target, ancilla)
         )
-
-    return _circuit_factory
 
 
 def discrimination_probability_upper_bound(
