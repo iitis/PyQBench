854eb389a6542fd25a27c9d3ae45d3973510507e
Aleksandra Krawiec <akrawiec@iitis.pl>
2022-07-05 17:44:04 +0200

    add classes DexiceDescription and ExperimentDescription and tests to these classes

---
+++ qbench/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class DeviceDescription(BaseModel):
+    anr: str
+    disable_qubit_rewiring: bool
+    gateset: str
+
+class ExperimentDescription(BaseModel):
+    type: str
+    qubits: list 
+    angle: dict 
+    method: str
+    number_of_shots: int
---
+++ tests/test_models.py
@@ -0,0 +1,61 @@
+from pydantic import ValidationError
+import pytest
+from qbench.models import DeviceDescription, ExperimentDescription
+
+
+class TestDeviceDescription:
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            {"arn": "test", "disable-qubit-rewiring": False, "gateset": "rigetti"}
+        ]
+    )
+    def test_can_be_parsed_from_correct_input(self, input):
+        description = DeviceDescription(**input)
+        assert description.arn == input["arn"]
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            {"disable-qubit-rewiring": True}
+        ]
+    )
+    def test_cannot_be_parsed_from_incorrect_input(self, input):
+        with pytest.raises(ValidationError):
+            DeviceDescription(**input)
+
+class TestExperimentDescription:
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            {
+                "type": "fourier_discrimination", 
+                "qubits": [
+                    {"target": 0, "ancilla": 1},
+                    {"target": 5, "ancilla": 2}
+                ],
+                "angle": {
+                    "start": 0,
+                    "stop": 4,
+                    "number_of_points": 3
+                },
+                "method": "postselection",
+                "number_of_shots": 5
+            }
+        ]
+    )
+    def test_can_be_parsed_from_correct_input(self, input):
+        description = ExperimentDescription(**input)
+        assert description.type == input["type"]
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            {"disable-qubit-rewiring": True}
+        ]
+    )
+    def test_cannot_be_parsed_from_incorrect_input(self, input):
+        with pytest.raises(ValidationError):
+            ExperimentDescription(**input)
