2d53774786fc1a47d9f4e57292e6dd732ef1e8aa
Konrad Jałowiecki <dexter2206@gmail.com>
2022-08-29 14:33:10 +0200

    Remove count_specific_measurements function

--- qbench/utils.py
+++ qbench/utils.py
@@ -1,16 +1,7 @@
 """Module containing utilities, maybe combinatorial ones."""
-from collections import Counter
 from typing import Dict
 
 from qiskit import QuantumCircuit, transpile
-
-
-def count_specific_measurements(measurement_counts: Counter, qubit_index: int, qubit_value: int):
-    return sum(
-        count
-        for bitstring, count in measurement_counts.items()
-        if int(bitstring[qubit_index]) == qubit_value
-    )
 
 
 def remap_qubits(circuit: QuantumCircuit, virtual_to_physical: Dict[int, int]):
