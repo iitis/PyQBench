6af37d72a736496a9fd64f60804bad3088c98fd8
Konrad Jałowiecki <dexter2206@gmail.com>
2022-09-16 00:32:39 +0200

    Remove verbatim related stuff from IBMQ tests

--- tests/test_fourier_ibmq.py
+++ tests/test_fourier_ibmq.py
@@ -7,11 +7,11 @@ from qiskit.circuit import Instruction
 from qbench.fourier import FourierComponents
 
 
-def _assert_can_be_run_in_verbatim_mode(backend, instruction: Instruction):
+def _assert_can_be_run(backend, instruction: Instruction):
     circuit = QuantumCircuit(instruction.num_qubits)
     circuit.append(instruction, list(range(instruction.num_qubits)))
     circuit.measure_all()
-    resp = backend.run(circuit.decompose(), shots=10, verbatim=True)
+    resp = backend.run(circuit.decompose(), shots=10)
     assert resp.result()
     assert sum(resp.result().get_counts().values()) == 10
 
@@ -35,13 +35,13 @@ def circuits():
 @pytest.mark.skipif("not config.getoption('ibmq')")
 class TestIBMQDeviceCanRunDecomposedCircuitsInVerbatimMode:
     def test_black_box_can_be_run(self, ibmq, circuits):
-        _assert_can_be_run_in_verbatim_mode(ibmq, circuits.black_box_dag)
+        _assert_can_be_run(ibmq, circuits.black_box_dag)
 
     def test_v0_dag_can_be_run(self, ibmq, circuits):
-        _assert_can_be_run_in_verbatim_mode(ibmq, circuits.v0_dag)
+        _assert_can_be_run(ibmq, circuits.v0_dag)
 
     def test_v1_dag_can_be_run(self, ibmq, circuits):
-        _assert_can_be_run_in_verbatim_mode(ibmq, circuits.v1_dag)
+        _assert_can_be_run(ibmq, circuits.v1_dag)
 
     def test_v0_v1_direct_sum_dag_can_be_run(self, ibmq, circuits):
-        _assert_can_be_run_in_verbatim_mode(ibmq, circuits.controlled_v0_v1_dag)
+        _assert_can_be_run(ibmq, circuits.controlled_v0_v1_dag)
