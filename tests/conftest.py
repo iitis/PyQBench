3c0cf3ee2a58534a0b06117974c68eebf73ae551
plewandowska <plewandowska@iitis.pl>
2022-09-15 11:51:27 +0200

    Add parser option for IBMQ

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,10 @@ def pytest_addoption(parser):
             "Note that to run these tests you need to have AWS CLI configured."
         ),
     )
+    parser.addoption(
+        "--ibmq",
+        action="store_true",
+        dest="ibmq",
+        default=False,
+        help=("Enable validation tests on actual IBMQ devices. "),
+    )
