fb9e70ab307739bcf1e8d740a85bda03648a954d
Konrad Jałowiecki <dexter2206@gmail.com>
2022-09-18 23:48:23 +0200

    Move missed fourier-specific models to their respective module

--- qbench/common_models.py
+++ qbench/common_models.py
@@ -77,11 +77,6 @@ class IBMQJobDescription(BaseModel):
     ibmq_job_id: str
 
 
-class ResultForAngle(BaseModel):
-    phi: float
-    histograms: Dict[str, Union[SynchronousHistogram, IBMQJobDescription]]
-
-
 def _import_object(object_spec):
     module_path, obj_name = object_spec.split(":")
     module = import_module(module_path)
