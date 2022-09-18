01b012d289f4b573bc2b90abf0c735468d6ca794
Konrad Jałowiecki <dexter2206@gmail.com>
2022-09-18 23:00:56 +0200

    Add logging implementation

---
+++ qbench/logging.py
@@ -0,0 +1,30 @@
+"""Implementation of colored logging and definitions of log format used by PyQBench."""
+import logging
+
+RESET = "\x1b[0m"
+
+COLOR_MAP = {
+    "INFO": "\x1b[32;20m",
+    "DEBUG": "\x1b[38;20m",
+    "WARNING": "\x1b[33;20m",
+    "ERROR": "\x1b[31;20m",
+    "CRITICAL": "\x1b[31;1m",
+}
+
+
+FORMAT = "%(colored_name)s | %(asctime)s | %(name)s | %(message)s"
+
+_default_logrecord_factory = logging.getLogRecordFactory()
+
+
+def _logrecord_factory(*args, **kwargs):
+    record = _default_logrecord_factory(*args, **kwargs)
+    color = COLOR_MAP[record.levelname]
+    record.colored_name = f"{color}{record.levelname}{RESET}"
+    return record
+
+
+def configure_logging():
+    logging.basicConfig(format=FORMAT)
+    logging.setLogRecordFactory(_logrecord_factory)
+    logging.getLogger("qbench").setLevel("INFO")
