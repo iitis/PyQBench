19953efd9576eb75d76f56a325bace1029e2db38
Konrad Jałowiecki <dexter2206@gmail.com>
2022-10-26 00:58:39 +0200

    Add basic documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from typing import List
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PyQBench"
+copyright = "2022, Konrad Jałowiecki, Paulina Lewandowska, Łukasz Pawela"
+author = "Konrad Jałowiecki, Paulina Lewandowska, Łukasz Pawela"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ["_templates"]
+exclude_patterns: List[str] = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
+myst_enable_extensions = ["colon_fence"]
---
+++ docs/source/mathematical_foundations.md
@@ -0,0 +1,4 @@
+# Mathematical foundations
+PyQBench is a package for benchmarking gate-based quantum computers by 
+estimating how well they can discriminate between certain measurements.
+
