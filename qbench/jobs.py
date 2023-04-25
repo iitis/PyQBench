26c4160e98eb86f15dd16291f9f54f641a31f67a
Konrad Jałowiecki <dexter2206@gmail.com>
2023-04-25 21:15:54 +0200

    Temporarily disable specialized job retrieval for IQMB backends (#48)

--- qbench/jobs.py
+++ qbench/jobs.py
@@ -3,7 +3,8 @@ from functools import singledispatch
 from typing import Sequence
 
 from qiskit.providers import JobV1
-from qiskit.providers.ibmq import IBMQBackend, IBMQJob
+
+# from qiskit.providers.ibmq import IBMQBackend, IBMQJob
 
 
 @singledispatch
@@ -18,6 +19,6 @@ def retrieve_jobs(backend, job_ids: Sequence[str]) -> Sequence[JobV1]:
     return [backend.retrieve_job(job_id) for job_id in job_ids]
 
 
-@retrieve_jobs.register
-def _retrieve_jobs_from_ibmq(backend: IBMQBackend, job_ids: Sequence[str]) -> Sequence[IBMQJob]:
-    return backend.jobs(db_filter={"id": {"inq": job_ids}}, limit=len(job_ids))
+# @retrieve_jobs.register
+# def _retrieve_jobs_from_ibmq(backend: IBMQBackend, job_ids: Sequence[str]) -> Sequence[IBMQJob]:
+#     return backend.jobs(db_filter={"id": {"inq": job_ids}}, limit=len(job_ids))
